d18b94f65f60c19dde351915ecc5962a1ab56f6e
unkn3wn <reyesfernando151@gmail.com>
2023-01-03 12:25:52 -0800

    working on reponsive same thing as last time this time footer

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -6,108 +6,87 @@ import ugo from "../assets/ugonna pfp.png";
 function Home() {
   const nav = useNavigate();
   return (
-    <div>
-      <div className="h-screen">
-        <div className={styles.front}>
-          <h1 className={styles.title}>Real Fake Clothes</h1>
-          <button
-            href="#_"
-            class="relative inline-flex items-center px-8 py-1 overflow-hidden text-lg font-medium text-black-300 border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 mt-14"
-            onClick={() => {
-              nav("/products");
-            }}
-          >
-            <span class="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-            <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
-              <svg
-                className="w-5 h-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </span>
-            <span class="relative p-4 ">Shop Now</span>
-          </button>
-        </div>
-
-        <div className="flex flex-row items-center justify-center text-white w-full absolute bottom-0 items-center bg-stone-600">
-          <div className="flex flex-col mt-10 mb-3 items-center space-x-10 ">
-            <img
-              className="flex justify-center relative rounded-full h-24 "
-              src={wilson}
-              alt="model"
-            />
-            <p className=" text-3xl justify-center mr-4">Wilson Chen</p>
+    <div className="h-screen">
+      <div className={styles.front}>
+        <h1 className={styles.title}>Real Fake Clothes</h1>
+        <button
+          href="#_"
+          class="relative inline-flex items-center px-8 py-1 overflow-hidden text-lg font-medium text-black border-2 border-black rounded-full hover:text-white group hover:bg-gray-50 mt-14"
+          onClick={() => {
+            nav("/products");
+          }}
+        >
+          <span class="absolute left-0 block w-full h-0 transition-all bg-black opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+          <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+            <svg
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M14 5l7 7m0 0l-7 7m7-7H3"
+              ></path>
+            </svg>
+          </span>
+          <span class="relative p-4 ">Shop Now</span>
+        </button>
+        <footer>
+          <div className={styles.cards}>
+            <img className={styles.profilePic} src={wilson} alt="model" />
+            <p className={styles.name}>Wilson Chen</p>
             <div>
-              <a
-                href="https://github.com/Wilsonrchen"
-                className="hover:text-pyellow p-2"
-              >
-                Github
+              <a href="https://github.com/Wilsonrchen" target="_blank" class>
+                <img className={styles.gitImg} src="githublogo.jpg" />
               </a>
               <a
                 href="https://www.linkedin.com/in/wilsonrchen/"
-                className="hover:text-pyellow p-2"
+                target="_blank"
+                
               >
                 {" "}
-                LinkedIn
+                <img  className={styles.linkLog} src="LinkedIn.jpg" />
               </a>
             </div>
           </div>
-          <div className="flex flex-col mt-10 mb-3 items-center space-x-10">
-            <img
-              className="flex items-center justify-center relative rounded-full h-24 "
-              src={ferni}
-              alt="model"
-            />
-            <p className=" text-3xl justify-center mr-4">Fernando Reyes</p>
+
+          <div className={styles.cards}>
+            <img className={styles.profilePic} src={ferni} alt="model" />
+            <p className={styles.name}>Fernando Reyes</p>
             <div>
-              <a
-                href="https://github.com/unkn3wn"
-                className="hover:text-pyellow p-2"
-              >
-                Github
+              <a href="https://github.com/unkn3wn" target="_blank">
+                <img className={styles.gitImg} src="githublogo.jpg" />
               </a>
               <a
                 href="https://www.linkedin.com/in/fernando-reyes-673860240/"
-                className="hover:text-pyellow p-2"
+                target="_blank"
               >
                 {" "}
-                LinkedIn
+                <img className={styles.linkLog} src="LinkedIn.jpg" />
               </a>
             </div>
           </div>
-          <div className="flex flex-col mt-10 mb-3 items-center space-x-10">
-            <img
-              className="flex items-center justify-center relative rounded-full h-24 "
-              src={ugo}
-              alt="model"
-            />
-            <p className=" text-3xl justify-center">Ugonna Duru</p>
+
+          <div className={styles.cards}>
+            <img className={styles.profilePic} src={ugo} alt="model" />
+            <p className={styles.name}> Ugonna Duru</p>
             <div>
-              <a
-                href="https://github.com/Uduru10"
-                className="hover:text-pyellow p-2"
-              >
-                Github
+              <a href="https://github.com/Uduru10" target="_blank">
+                <img className={styles.gitImg} src="githublogo.jpg" />
               </a>
               <a
                 href="https://www.linkedin.com/in/ugo-duru-780aa6257/"
-                className="hover:text-pyellow p-2"
+                target="_blank"
               >
                 {" "}
-                LinkedIn
+                <img className={styles.linkLog} src="LinkedIn.jpg" />
               </a>
             </div>
           </div>
-        </div>
+        </footer>
       </div>
     </div>
   );
