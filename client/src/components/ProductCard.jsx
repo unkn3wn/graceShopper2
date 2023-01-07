60fef4210401262b555c67d9670208c593e3c473
unkn3wn <reyesfernando151@gmail.com>
2023-01-07 11:32:12 -0800

    fixing the productss/products cards/anything with products might get rid of discription

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -9,80 +9,57 @@ import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutl
 
 export default function ProductCard({ product, setError }) {
   const { addToCart, cart } = useCart();
+  // what i got rid of need to add back
+  // {product.price}
 
   return (
     <div className={styles.allProducts} key={product}>
-      <Card sx={{ width: 290, height: 400 }}>
+      <Card sx={{ width: 370, height: 350 }}>
         <Typography level="h5" fontSize="md" sx={{ mb: 1 }}>
           {product.name}
         </Typography>
-        <div className="group h-64 w-64 [perspective:1000px]">
-          <div
-            className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500
-             [transform-style:preserve-3d]
-          group-hover:[transform:rotateY(180deg)]"
-          >
-            <div className="absolute inset-0">
-              <img
-                className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-                src={product.imageUrl}
-              />
-            </div>
-            <div
-              className="absolute inset-0 h-full w-full rounded-xl bg-black/40 px-12 text-center text-white
-            [transform:rotateY(180deg)] [backface-visibility:hidden] "
-            >
-              <div>
-                {product.description}{" "}
-                
-              </div>{" "}
-            </div>
-          </div>
+
+        <div className={styles.imgDescription}>
+          {product.description}{" "}
+          <img className={styles.imgProducts} src={product.imageUrl} />
         </div>
 
-        <Box sx={{ display: "flex", mt: 3 }}>
-          <div>
-            <Typography level="body4">Total price:</Typography>
-            <Typography fontSize="lg" fontWeight="lg">
-              ${product.price}
-            </Typography>
-            <Button
-                  className="focus:ring focus:ring-customyellow"
-                  variant="contained"
-                  onClick={async () => {
-                    try {
-                      await addToCart({
-                        order_id: cart.id,
-                        product_id: product.id,
-                        quantity: 1,
-                      });
-                      setError("");
-                    } catch (err) {
-                      setError(err.response.data.message);
-                    }
-                  }}
-                  sx={{
-                    color: "black",
-                    background: "yellow",
-                    width: 128,
-                    height: 50,
-                    display: "inline",
-                    fontWeight: "bold",
-                    mx: "auto",
-                    mt: 4,
-                    fontSize: 10,
-                    "&:hover": {
-                      color: "white",
-                      background: "black",
-                    },
-                    hover: { color: "black" },
-                  }}
-                >
-                  add to cart
-                  <AddShoppingCartOutlinedIcon />
-                </Button>
-          </div>
-        </Box>
+        <div className={styles.pricingButtonn}>
+          <h1>Price:$ {product.price}</h1>
+          <Button
+            className="focus:ring focus:ring-customyellow"
+            variant="contained"
+            onClick={async () => {
+              try {
+                await addToCart({
+                  order_id: cart.id,
+                  product_id: product.id,
+                  quantity: 1,
+                });
+                setError("");
+              } catch (err) {
+                setError(err.response.data.message);
+              }
+            }}
+            sx={{
+              color: "black",
+              background: "yellow",
+              width: 110,
+              height: 30,
+              display: "inline",
+              fontWeight: "bold",
+              fontSize: 10,
+              "&:hover": {
+                color: "black",
+                background: "yellow",
+              },
+              hover: { color: "black" },
+            }}
+          >
+            add to cart
+            <AddShoppingCartOutlinedIcon />
+          </Button>
+        </div>
       </Card>
     </div>
   );
